35541efa9b7bf0ff9af3164a9689d55357f4a9f9
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-07-24 17:38:42 -0600

    Added setup.py script

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,1 +1,9 @@
+from src.analysis.generic import Generic
+from src.analysis.cold import COLDAnalysis
+from src.analysis.hatecheck import HateCheckAnalysis
 
+__all__ = [
+    "Generic" , 
+    "COLDAnalysis" , 
+    "HateCheckAnalysis"
+]
--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,15 @@
-# This is empty for now.
+from src.data.cold import COLD , COLDSubmissionObject
+from src.data.dataset import Dataset
+from src.data.dso import DatasetSubmissionObject
+from src.data.hatecheck import HateCheck , HateCheckSubmissionObject
+from src.data.hatexplain import HateXplain , HateXplainSubmissionObject
+
+__all__ = [
+    
+    "Dataset", "DatasetSubmissionObject",
+    "COLD" , "COLDSubmissionObject" , 
+    "HateCheck" , "HateCheckSubmissionObject" ,
+    "HateXplain" , "HateXplainSubmissionObject"
+]
+
+
