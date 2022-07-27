7a7497956787a34b03ca2a61952d3416c1fdfebe
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-07-27 16:59:26 -0600

    Pip now also contains twitteraae code

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.8' 
 AUTHORS = 'OLEA Team, Anonymized for Review'
 DESCRIPTION = 'Short OLEA description'
 LONG_DESCRIPTION = 'Longer OLEA description'
 
 print('Finding packages...')
-print(find_packages(where='olea' , 
-                    exclude=['unittests*' , 'experiments*']))
+print(find_packages(where='.' , 
+                    include=['olea*'] , 
+                    exclude=['*unittests*' , '*experiments*']))
 
 # find_packages(where='src' , 
 #                                 exclude=['unittests*' , 'experiments*'])
@@ -20,7 +21,11 @@ setup(
         author_email = "<olea.ask@gmail.com>",
         description = DESCRIPTION,
         long_description = LONG_DESCRIPTION,
-        packages = ['olea'],
+        packages = find_packages(where='.' , 
+                    include=['olea*'] , 
+                    exclude=['*unittests*' , '* experiments*']),
+        package_data={'olea.utils.twitteraae' : ['model/*.txt']}, 
+        include_package_data=True,
         install_requires = ['numpy>1.21.0' , 
                             'scipy>1.6.0' , 
                             'datasets>2.2.0' , 
